0e8f85a7bb2ae194de318dac9ef910b9abca0468
vonrims <mr.mytab@gmail.com>
2026-05-29 12:38:59 +0100

    loop with attempts added

--- guess-game/src/main.rs
+++ guess-game/src/main.rs
@@ -8,20 +8,24 @@ fn main() {
     let secret_num = rand::thread_rng().gen_range(0..=10);
 
     println!("...secret number was generated. Try to guess it in arange between 0 and 10");
+    
+    loop {
 
-    let mut user_input = String::new();
+        println!("\nPlease input your guess:");
+    
+        let mut user_input = String::new();
+    
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("...enter a number");
+    
+        let user_input: u32 = user_input.trim().parse().expect("...enter a number");
 
-    io::stdin().read_line(&mut user_input).expect("...enter a number!");
-
-    let user_input: u32 = user_input.trim().parse().expect("...enter a number");
-
-    match secret_num.cmp(&user_input){
-        Ordering::Less => println!("Less than a number"),
-        Ordering::Greater => println!("Greater than a number"),
-        Ordering::Equal => println!("Hooray! Equal to a number!")
+        match user_input.cmp(&secret_num){
+            Ordering::Less => println!("Less than a number"),
+            Ordering::Greater => println!("Greater than a number"),
+            Ordering::Equal => println!("Hooray! Equal to a number!")
+        }
     }
 
-    // println!("Your input was: {user_input}");
-
-    // println!("...and the secrete number was: {secret_num}");
 }
