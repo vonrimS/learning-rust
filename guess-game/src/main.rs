bec403240e9b1ddd9dc3f783c6caff20264f370e
vonrims <mr.mytab@gmail.com>
2026-05-29 12:13:09 +0100

    user input added

--- guess-game/src/main.rs
+++ guess-game/src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello! Let's play a Guess game!");
+    println!("Enter your input: ");
+
+    let mut user_input = String::new();
+
+    io::stdin().read_line(&mut user_input).expect("...enter a number!");
+
+    println!("Your input was: {user_input}");
 }
