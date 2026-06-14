0562d0cac9c6bf0f7aa3880f517ff48c0c8bba80
vonrims <mr.mytab@gmail.com>
2026-06-14 22:20:25 +0100

    currency-exchange refactoring

--- currency-exchange/src/main.rs
+++ currency-exchange/src/main.rs
@@ -1,4 +1,4 @@
-use text_io::{read};
+use std::io;
 use rust_decimal::Decimal;
 
 // Read an amount and an exchange rate from input.
@@ -7,13 +7,17 @@ use rust_decimal::Decimal;
 // Result: [result]
 
 fn main() {
-    println!("Welcome to our Currency Exchange app!");
-    
-    println!("...enter amount of money:");
-    let ammount: Decimal = read!();
-    
-    println!("...enter exhange rate:");
-    let rate: Decimal = read!();
+    println!("Welcome to our Currency Exchange app!");    
 
-    println!("Result: {:.2}", ammount * rate);
+    let amount: Decimal = read_decimal_from_user("How much:");
+    let rate: Decimal = read_decimal_from_user("What's the rate:");
+
+    println!("Result: {:.2}", amount * rate);
+}
+
+fn read_decimal_from_user(s: &str) -> Decimal{
+    println!("{s}");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("...cannot read your input");
+    input.trim().parse().expect("...cannot parse your input")    
 }
