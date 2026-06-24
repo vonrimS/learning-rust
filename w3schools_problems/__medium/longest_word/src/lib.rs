a3f5228e40b3a467751105e52bc639896036fff6
vonrims <mr.mytab@gmail.com>
2026-06-25 00:24:53 +0100

    longest_word done

---
+++ w3schools_problems/__medium/longest_word/src/lib.rs
@@ -0,0 +1,57 @@
+// Read a sentence from input.
+// Find the longest word and print it.
+// If two words have the same length, print the one that appears first.
+// Words are separated by single spaces.
+
+use std::io;
+
+pub fn read_input() -> String{
+    let mut input = String::new();
+
+    loop {
+        input.clear();
+
+        io::stdin().read_line(&mut input).expect("...cannot read your input");
+        let trimmed = input.trim();
+
+        if is_valid(trimmed) {
+            break trimmed.to_string();
+        }
+
+        println!("...invalid input. Try again:");
+    }
+}
+
+
+pub fn is_valid(s: &str) -> bool{
+    !s.is_empty() && s.chars().all(|c| c.is_ascii_alphabetic() || c.is_ascii_whitespace())
+}
+
+pub fn find_longest_word(s: &str) -> String {
+    let mut longest = String::new();
+
+    for w in s.split(' ') {
+        if w.len() > longest.len() {
+            longest = w.to_string()
+        }
+    }
+
+    longest
+}
+
+
+
+
+#[cfg(test)]
+
+mod tests{
+    use super::*;
+
+    #[test]
+    fn test_find_longest_word(){
+        assert_eq!(find_longest_word("a bb ccc"), "ccc");
+        assert_eq!(find_longest_word("a bb cc"), "bb");
+        assert_eq!(find_longest_word("a b c"), "a");
+        assert_eq!(find_longest_word("a  bbb  cc  ddd"), "bbb");
+    }
+}
---
+++ w3schools_problems/__medium/longest_word/src/main.rs
@@ -0,0 +1,6 @@
+use longest_word::*;
+
+fn main(){
+    let pharase = read_input();
+    println!("Longest: {}", find_longest_word(&pharase));
+}
